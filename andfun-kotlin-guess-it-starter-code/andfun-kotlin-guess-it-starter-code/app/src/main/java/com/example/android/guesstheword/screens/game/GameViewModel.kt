a053ef457bf5607385f03f32307be6c1d1fddd73
patrick <patrickchow0803@gmail.com>
2019-12-23 22:01:15 -0500

    Added backing properties to score and word variables.

--- andfun-kotlin-guess-it-starter-code/andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ andfun-kotlin-guess-it-starter-code/andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,16 +1,36 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel(){
 
+    /*
+    ---------------------------------
+        IN ORDER TO ACHIEVE encapsulation, use backing properties.
+        Backing properties means that there's an internal version and external version of variables.
+        _variableName = Internal version of the code. Can be Read and Written within the file.
+        variableName = External version of the code. Can only be read.
+
+        YOU WANT TO USE THE INTERNAL VERSION WHEN USING THE VARIABLE INSIDE OF IT'S OWN CLASS.
+        YOU WANT TO USE THE EXTERNAL VERSION WHEN USING THE VARIABLE OUTSIDE OF IT'S OWN CLASS.
+
+    ---------------------------------
+     */
+
     // The current word
-    var word = MutableLiveData<String>()
+    private val _word = MutableLiveData<String>()
+    val word : LiveData<String>
+        get() = _word
 
     // The current score. All LiveData start out as null. Therefore initialize it in the init block
-    var score = MutableLiveData<Int>()
+    // These three lines of code make it so that _score is known as a MutableLiveData (Can be changed)
+    // in the GameViewModel but score is seen as a LiveData (Can't be changed, only read)
+    private val _score = MutableLiveData<Int>()
+    val score : LiveData<Int>
+        get() = _score
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
@@ -21,7 +41,7 @@ class GameViewModel : ViewModel(){
         resetList()
         nextWord()
 
-        score.value = 0
+        _score.value = 0
     }
 
     override fun onCleared() {
@@ -67,19 +87,19 @@ class GameViewModel : ViewModel(){
         if (wordList.isEmpty()) {
             //gameFinished()
         } else {
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
     }
 
     fun onSkip() {
 //        score--
         // Null Check since live data starts out as null, get an error.
-        score.value = (score.value)?.minus(1)
+        _score.value = (score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score.value = (score.value)?.plus(1)
+        _score.value = (score.value)?.plus(1)
         nextWord()
     }
 }
